fe5c6c824ab4297da6aaa2d7d60a9b3bd0f5f12d
Americano <caua.americano0710@gmail.com>
2026-09-21 18:14:58 -0300

    Alterar a a validação da resposta usuario === resposta certa com inserção da pergunta no banco ao gerar quiz e depois consulta para validar se a resposta está correta

---
+++ app/api/buscar-quiz/[id]/route.ts
@@ -0,0 +1,19 @@
+import { pegarUsuarioId } from "@/lib/auth"
+import { buscarQuiz } from "@/lib/pergunta.model"
+
+export async function GET(_req: Request, {params}:{params: Promise<{id: string}>}){
+    try{
+        const {id} = await params
+        const quizId = Number(id)
+        const usuarioId = await pegarUsuarioId()
+        const resultado = await buscarQuiz(quizId, usuarioId)
+        if(resultado.length ===0){
+            return Response.json({mensagem: 'Quiz não encontrado'}, {status: 404})
+        }
+        return Response.json({resultado}, {status:200})
+    }catch(err){
+        console.error(err)
+        return Response.json({mensagem: "não deu certo a requisição"}, {status:500})
+    }
+
+}
